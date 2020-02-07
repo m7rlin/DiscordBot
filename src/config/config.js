4640838fd7c22bc8bf21b2212c4a3ff2fada4a25
m7rcin <merlin@magictm.com>
2020-02-07 17:23:27 +0100

    update token

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  token: "___YOUR_TOKEN_HERE___",
+  // token: "___YOUR_TOKEN_HERE___",
+  token: "NjUyNzU0NjA4NzY2MzIwNjQx.Xj2O1A.Gke35XaOOoyt_yrcAQdpcZeae1o",
   prefix: "!",
 }
