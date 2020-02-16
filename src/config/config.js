c53235da20bdf2d63465834ffec56d7728cfa21f
m7rcin <merlin@magictm.com>
2020-02-16 05:56:40 +0100

    Add dotenv - support for enviromental variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
+const dotenv = require("dotenv").config()
+
 module.exports = {
-  // token: "___YOUR_TOKEN_HERE___",
-  token: "NjUyNzU0NjA4NzY2MzIwNjQx.Xj2O1A.Gke35XaOOoyt_yrcAQdpcZeae1o",
+  token: process.env.TOKEN,
   prefix: "!",
 }
