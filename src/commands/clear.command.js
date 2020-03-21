702b9bca554ef7f1ab0598fe1ff4b27b1dcfa014
m7rcin <merlin@magictm.com>
2020-03-21 13:49:48 +0100

    Add permissions check for clear command

--- src/commands/clear.command.js
+++ src/commands/clear.command.js
@@ -1,13 +1,25 @@
+const {
+  Permissions: { FLAGS },
+} = require("discord.js")
+
 module.exports = {
   name: "clear",
   description: "Clear number of messages in specific channel.",
   args: true,
   usage: "<amount>",
+  botPermissions: [],
+  userPermissions: [],
 
   run(msg, args) {
-    const { channel } = msg
+    const { channel, guild, member } = msg
 
     const amountArg = parseInt(args[0])
+
+    console.log("bot", guild.me.permissions.has([FLAGS.ADMINISTRATOR]))
+    console.log("user", member.permissions.has([FLAGS.MANAGE_MESSAGES]))
+
+    if (!member.permissions.has([FLAGS.MANAGE_MESSAGES]))
+      return msg.reply("you need more permissions to execute this command!")
 
     if (!Number.isInteger(amountArg)) {
       return channel.send("You must specify the amount of messages to clear!")
