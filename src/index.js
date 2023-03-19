5fe238459ffee08fb03ccc3229e787fa0cd7ef3f
m7rlin <merlin@magictm.com>
2023-03-20 00:11:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
-const { Client, GatewayIntentBits } = require('discord.js')
+const { Client, GatewayIntentBits, REST, Routes } = require('discord.js')
 require('dotenv').config()
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'Replies with Pong!',
+    },
+]
+
+const TOKEN = process.env.TOKEN
+const CLIENT_ID = '1087096405899874377'
+
+// const rest = new REST({ version: '10' }).setToken(TOKEN)
+
+// ;(async () => {
+//     try {
+//         console.log('Started refreshing application (/) commands.')
+
+//         // await rest.put(
+//         //     Routes.applicationGuildCommands(CLIENT_ID, '594198055352401923'),
+//         //     {
+//         //         body: commands,
+//         //     },
+//         // )
+
+//         await rest
+//             .put(Routes.applicationCommands(CLIENT_ID), { body: [] })
+//             .catch(console.error)
+
+//         console.log('Successfully reloaded application (/) commands.')
+//     } catch (error) {
+//         console.error(error)
+//     }
+// })()
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
@@ -15,4 +48,4 @@ client.on('interactionCreate', async (interaction) => {
     }
 })
 
-client.login(process.env.TOKEN)
+client.login(TOKEN)
