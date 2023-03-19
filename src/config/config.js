5db9812fdc88f8da7c6ced577c1dc278f19d4b6d
m7rlin <merlin@magictm.com>
2023-03-19 21:23:45 +0100

    initial setup

--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require("dotenv").config({ path: __dirname + "/./../../.env" })
-
-module.exports = {
-  TOKEN: process.env.token,
-  PREFIX: process.env.prefix,
-  OWNER: process.env.owner,
-  CORONA_API: "https://corona.lmao.ninja/v2",
-}
--- src/index.js
+++ src/index.js
@@ -1,75 +1,18 @@
-const { Client } = require("discord.js")
-const chalk = require("chalk")
+const { Client, GatewayIntentBits } = require('discord.js')
+require('dotenv').config()
 
-const { TOKEN } = require("./config/config.js")
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-const client = new Client()
-
-const commandHandler = require("./handlers/command.handler")
-const settingsHandler = require("./handlers/settings.handler")
-const apiHandler = require("./handlers/api.handler")
-const eventHandler = require("./handlers/event.handler")
-
-const log = console.log
-
-// Initialize Comamnd Handler
-commandHandler(client)
-// Initialize Settings Handler
-settingsHandler(client)
-// Initialize API Handler
-apiHandler(client)
-// Initialize Event Handler
-eventHandler(client)
-
-client.on("ready", () => {
-  log(chalk.green(`Zalogowano jako ${client.user.tag}!`))
-
-  // ========== TEST
-  // User joined guild
-  // client.emit("guildMemberAdd", client.guilds.cache.get("358614500758257665").members.cache.get("257954238339088384"))
-  // User left guild
-  // client.emit("guildMemberRemove", client.guilds.cache.get("358614500758257665").members.cache.get("257954238339088384"))
-
-  // Initialize interval for each guild
-  client.settings.forEach((config, guildId) => {
-    const { guilds } = client
-    // Check if guild exist
-    if (guilds.cache.has(guildId)) {
-      const guild = guilds.cache.get(guildId)
-      // Check if available
-      if (guild.available) {
-        // console.log("available")
-
-        // Set Interval for each channel
-        const clockChannels = config.clocks
-        setInterval(() => {
-          const time = new Date().toLocaleTimeString().slice(0, 5)
-          const channelName = `🕥 ${time}`
-
-          clockChannels.forEach((channelId, index) => {
-            // Check if channel exists
-            if (guild.channels.cache.has(channelId)) {
-              // log("channel exist")
-              const channelToUpdate = guild.channels.cache.get(channelId)
-              channelToUpdate.setName(channelName)
-            } else {
-              // log("not exist")
-              // Remove Id from config
-              // that does not exist
-              clockChannels.splice(index, 1)
-              client.saveConfig(guildId)
-            }
-          })
-        }, 60 * 1000)
-      }
-    }
-  })
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
 })
 
-// Connect with Discord
-client.login(TOKEN)
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isChatInputCommand()) return
 
-// Error handler - omit crashed
-client.on("debug", () => {})
-client.on("warn", () => {})
-client.on("error", () => {})
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!')
+    }
+})
+
+client.login(process.env.TOKEN)
