bbc896bdfe41c2726b14ccddf4168a2a06acea5d
m7rlin <merlin@magictm.com>
2023-03-20 04:34:55 +0100

    changes

--- src/config.js
+++ src/config.js
@@ -1,1 +1,6 @@
-module.exports = {}
+require('dotenv').config()
+
+module.exports = {
+    TOKEN: process.env.TOKEN,
+    CLIENT_ID: process.env.CLIENT_ID,
+}
--- src/index.js
+++ src/index.js
@@ -1,51 +1,23 @@
-const { Client, GatewayIntentBits, REST, Routes } = require('discord.js')
-require('dotenv').config()
+const { Client, GatewayIntentBits } = require('discord.js')
 
-const commands = [
-    {
-        name: 'ping',
-        description: 'Replies with Pong!',
-    },
-]
+const { TOKEN, CLIENT_ID } = require('./config')
 
-const TOKEN = process.env.TOKEN
-const CLIENT_ID = '1087096405899874377'
-
-// const rest = new REST({ version: '10' }).setToken(TOKEN)
-
-// ;(async () => {
-//     try {
-//         console.log('Started refreshing application (/) commands.')
-
-//         // await rest.put(
-//         //     Routes.applicationGuildCommands(CLIENT_ID, '594198055352401923'),
-//         //     {
-//         //         body: commands,
-//         //     },
-//         // )
-
-//         await rest
-//             .put(Routes.applicationCommands(CLIENT_ID), { body: [] })
-//             .catch(console.error)
-
-//         console.log('Successfully reloaded application (/) commands.')
-//     } catch (error) {
-//         console.error(error)
-//     }
-// })()
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] })
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`)
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
 })
 
-client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isChatInputCommand()) return
+client.on('ready', () => {
+    console.log(`Zalogowano jako ${client.user.tag}!`)
+})
 
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!')
-    }
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return
+
+    message.reply('Hej, oto twoja wiadomość: ' + message.content)
 })
 
 client.login(TOKEN)
