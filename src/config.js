f63a9c1ef2e715c9614812fa35ce48c414085c17
m7rlin <merlin@magictm.com>
2023-06-25 16:17:43 +0200

    Revert "changes"
    This reverts commit d95495a92575cb144f15f3cc6c42f73ec90477c1.

--- src/config.js
+++ src/config.js
@@ -3,5 +3,4 @@ require('dotenv').config()
 module.exports = {
     TOKEN: process.env.TOKEN,
     CLIENT_ID: process.env.CLIENT_ID,
-    GUILD_ID: process.env.GUILD_ID,
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-const { Client, GatewayIntentBits, Events, Collection } = require('discord.js')
-const fs = require('node:fs')
-const path = require('node:path')
-
-// const registerCommands = require('./register-commands')
+const { Client, GatewayIntentBits } = require('discord.js')
 
 const { TOKEN } = require('./config')
 
@@ -14,47 +10,32 @@ const client = new Client({
     ],
 })
 
-client.commands = new Collection()
+const PREFIX = '!'
 
-client.cooldowns = new Collection()
+client.on('ready', () => {
+    console.log(`Zalogowano jako ${client.user.tag}!`)
+})
 
-const foldersPath = path.join(__dirname, 'commands')
-const commandFolders = fs.readdirSync(foldersPath)
+client.on('messageCreate', (message) => {
+    // Bot message
+    if (message.author.bot) return
 
-// Register commands
-for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder)
-    const commandFiles = fs
-        .readdirSync(commandsPath)
-        .filter((file) => file.endsWith('.js'))
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file)
-        const command = require(filePath)
-        // Set a new item in the Collection with the key as the command name and the value as the exported module
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command)
-        } else {
-            console.log(
-                `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-            )
-        }
-    }
-}
+    // Ignore messages without prefix
+    if (!message.content.startsWith(PREFIX)) return
 
-// Register events
-const eventsPath = path.join(__dirname, 'events')
-const eventFiles = fs
-    .readdirSync(eventsPath)
-    .filter((file) => file.endsWith('.js'))
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/g)
+    const commandName = args.shift().toLowerCase()
 
-for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file)
-    const event = require(filePath)
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args))
+    console.log(commandName, args)
+
+    if (commandName === 'info') {
+        message.reply('Nazna komendy: ' + commandName)
+        message.reply('Argumenty: ' + args)
+    } else if (commandName === 'ping') {
+        message.reply('Pong!')
     } else {
-        client.on(event.name, (...args) => event.execute(...args))
+        message.reply('Nie ma takiej komendy!')
     }
-}
+})
 
 client.login(TOKEN)
